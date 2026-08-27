825527929068eca0a6874927d6707de2bbaa043a
weeakweeak1-star <weeakweeak1@gmail.com>
2026-08-27 22:09:46 +0300

    Manual deploy UI overhaul

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3877723812"
+    serviceWorkerVersion: "1845065339"
   }
 });
